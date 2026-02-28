021bd66dd1f07ac39375019b1c7988649e4319b2
zaheersha <a.zaheersha@gmail.com>
2026-02-28 22:44:57 +0530

    responsive

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), // 👈 This fixes "@/...." imports
+    },
+  },
+
   build: {
     rollupOptions: {
       input: {
